52f2b2d9f9acd4c71e91d412d1c247345d66a22e
thaiKari <karimelingjohannessen@gmail.com>
2018-09-19 09:06:17 +0200

    styling works better now

--- src/components/DragNDropBox.js
+++ src/components/DragNDropBox.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import FileUpload from './FileUpload';
 import { Typography } from '../../node_modules/@material-ui/core';
+import classNames from 'classnames';
 
 const styles = theme => ({
     dropBox: {
@@ -13,22 +14,34 @@ const styles = theme => ({
         verticalAlign: 'middle',
         padding: 20,
         paddingTop: 50
-    }
+    },
+    dropBoxActive: {
+      backgroundColor: theme.palette.action.hover
+  }
   
   });
 
   class DragNDropBox extends Component {
+    state = {
+      dropBoxActivated: false
+    };
     
     render() {
 
       const { classes, receiveNewJson, theme } = this.props;
+      const { dropBoxActivated } = this.state;
+
+      var liClasses = classNames({
+        [classes.dropBox]: true,
+        [classes.dropBoxActive]: dropBoxActivated
+      });
   
       return (
         <FileUpload receiveNewJson={receiveNewJson} disableClick>
-            <div className={classes.dropBox}
-                onDragOver={(e)=> e.target.style.backgroundColor=theme.palette.action.hover}
-                onDragLeave={(e)=> e.target.style.backgroundColor=theme.palette.background.paper}
-                onDrop={(e)=> e.target.style.backgroundColor=theme.palette.background.paper}>
+            <div className={liClasses}
+                onDragOver={()=> this.setState({dropBoxActivated: true})}
+                onDragLeave={()=> this.setState({dropBoxActivated: false})}
+                onDrop={()=> this.setState({dropBoxActivated: false})}>
                 
                 <Typography> Drag and drop a geojson file here</Typography>
             </div>
