925ec11183003ab7615481cd6eaad6d2ea76a581
thaiKari <karimelingjohannessen@gmail.com>
2018-10-07 18:50:34 +0200

    moved layer selection up from layerlist to LayerBar

--- src/layout/LayerBar.js
+++ src/layout/LayerBar.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { Drawer } from '@material-ui/core';
-
+import findIndexWithAttribute from '../utils/findIndexWithAttribute'
 import DragNDropBox from '../components/DragNDropBox';
 import LayerList from '../components/LayerList';
 import LayersToolbar from '../components/LayersToolbar';
@@ -26,7 +26,13 @@ const styles = theme => ({
   class LayerBar extends Component {
     constructor() {
       super()
-      this.state = { files: [] }
+      this.state = { 
+        files: [],
+        selectedLayers: {},
+        ctrlPressed: false,
+        shiftPressed: false,
+      }
+        this.onDragEnd = this.onDragEnd.bind(this);
     }
   
     onDrop(files) {
@@ -34,10 +40,136 @@ const styles = theme => ({
         files
       });
     }
+
+    onDragEnd(result) {
+      if (!result.destination) {
+        return;
+      }
+
+      const { reorderLayersList } = this.props;
+      reorderLayersList( result.source.index, result.destination.index);
+  
+    }
+
+    componentDidMount() {
+      this.layersChange();
+      document.addEventListener('keydown',this.keydownHandler.bind(this));
+      document.addEventListener('keyup',this.keyupHandler.bind(this));
+    }
+    componentWillUnmount(){
+      document.removeEventListener('keydown',this.keydownHandler.bind(this));
+      document.removeEventListener('keyup',this.keyupHandler.bind(this));
+    }
+
+    keydownHandler(e){
+      if(e.key === 'Control'){
+        this.setState({ctrlPressed: true});
+      }
+      if(e.key === 'Shift'){
+        this.setState({shiftPressed: true});
+      }
+    }
+
+    keyupHandler(e){
+      if(e.key === 'Control'){
+        this.setState({ctrlPressed: false});
+      }
+      if(e.key === 'Shift'){
+        this.setState({shiftPressed: false});
+      }
+    }
+
+
+    componentDidUpdate = (prevProps) => {
+
+      if(prevProps.layersChange !== this.props.layersChange){
+        this.layersChange();
+      }
+        
+    }
+
+    layersChange = () => {
+      let {selectedLayers} = this.state;
+      let {layers} = this.props;
+
+      let newSelectedLayers = layers.reduce(
+        (selected, layer) => {
+
+          if(! selected[layer.id] ) {
+            selected[layer.id] = false;
+          }
+
+          return selected
+        },
+        selectedLayers
+      );
+
+      this.setState({selectedLayers: newSelectedLayers});
+    }
+
+    selectLayersBetween(lastClickedLayer, layerId, selectedLayers) {
+      const {layers} = this.props;
+      var fromIndex, toIndex;
+      let i1 = findIndexWithAttribute(layers, 'id', lastClickedLayer);
+      let i2 = findIndexWithAttribute(layers, 'id', layerId);
+
+      if (i1 < i2 ) {
+        fromIndex = i1;
+        toIndex = i2;
+      } else {
+        fromIndex = i2;
+        toIndex = i1;
+      }
+
+      for (var i = fromIndex; i <= toIndex; i++) {
+      
+        var id = layers[i].id;
+        selectedLayers[id] = true;
+      }
+
+      return selectedLayers;
+
+    }
+
+    handleListItemClick = (layerId) => {
+      
+        let {selectedLayers, ctrlPressed, shiftPressed, lastClickedLayer} = this.state;
+        const {layers} = this.props;
+
+        if (ctrlPressed) {
+          selectedLayers[layerId] = !selectedLayers[layerId] 
+        }
+        else if (shiftPressed) {
+          if (lastClickedLayer) {
+            selectedLayers = this.selectLayersBetween(lastClickedLayer, layerId, selectedLayers)
+          } else {
+            selectedLayers = this.selectLayersBetween(layers[0].id, layerId, selectedLayers)
+          }
+         
+        }
+        else {
+          Object.keys(selectedLayers).forEach(key => {
+            selectedLayers[key] = key === layerId ?
+            ! selectedLayers[key] : false;
+          });
+        }
+
+        this.setState({ 
+          selectedLayers: selectedLayers,
+          lastClickedLayer: layerId  });
+    };
+    
     
     render() {
 
-      const {addLayers, reorderLayersList, layersChange, layers, classes, drawerOpen, receiveNewJson, toggleVisibility} = this.props;
+      const {addLayers,
+        reorderLayersList,
+        layers,
+        classes,
+        drawerOpen,
+        receiveNewJson,
+        toggleVisibility} = this.props;
+      const {selectedLayers} = this.state
   
       return (
 
@@ -53,7 +185,12 @@ const styles = theme => ({
         <LayersToolbar addLayers={addLayers}/>
   
         <div className={classes.content}>
-          <LayerList reorderLayersList={reorderLayersList} layersChange={layersChange} layers={layers} toggleVisibility={toggleVisibility}/>
+          <LayerList reorderLayersList={reorderLayersList}
+                    layers={layers}
+                    toggleVisibility={toggleVisibility}
+                    handleListItemClick={this.handleListItemClick.bind(this)}
+                    onDragEnd={this.onDragEnd.bind(this)}
+                    selectedLayers={selectedLayers}/>
           <DragNDropBox receiveNewJson={receiveNewJson}/>
         </div>
 
